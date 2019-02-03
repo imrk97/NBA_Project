492eaabc6584af9b743faf62b46d4de51ef2beb4
Rohan Karmakar <rohankarmakar97@gmail.com>
2019-02-04 02:06:09 +0530

    minor changes

--- web/JS/main.js
+++ web/JS/main.js
@@ -1,4 +1,9 @@
-function feedbackValidation() { }
+function feedbackValidation() {
+    var cust_name=document.getElementById("name").value.trim();
+    var cust_email=document.getElementById("email").value.trim();
+    var cust_feedback=document.getElementById("feedback").value.trim();
+}
+
 function adminValidation() {
     const def_username = "admin";
     const def_password = "admin";
@@ -16,38 +21,3 @@ function adminValidation() {
         return false;
     }
 }
-
-
-
-/*function createCookie(name, value, days) {
-  var expires = "";
-   if (days) {
-        var date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toGMTString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function readCookie(name) {
-    var nameEQ = name + "=";
-    var ca = document.cookie.split(';');
-    for (var i = 0; i < ca.length; i++) {
-        var c = ca[i];
-        while (c.charAt(0) == ' ') {
-            c = c.substring(1, c.length);
-        }
-        if (c.indexOf(nameEQ) == 0) {
-            return c.substring(nameEQ.length, c.length);
-        }
-    }
-    return null;
-}
-
-function eraseCookie(name) {
-    createCookie(name, "", -1);
-}
-
-function preventBack(){window.history.forward();}
-    setTimeout("preventBack()", 0);
-    window.onunload=function(){null};*/
