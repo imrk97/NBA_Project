ad54b828d0b9baaab36e28d37d220f0ee39ad4e2
Sayan216016 <mukherjeesayan091@gmail.com>
2019-02-06 22:53:07 +0530

    connection is moved to a js file for sequrity reason

---
+++ public/JS/connection.js
@@ -0,0 +1,9 @@
+var config = {
+    apiKey: "AIzaSyD4Aq8pjCISVuQYei0t1fPBV2_BpWq3SNo",
+    authDomain: "nbacanteen.firebaseapp.com",
+    databaseURL: "https://nbacanteen.firebaseio.com",
+    projectId: "nbacanteen",
+    storageBucket: "nbacanteen.appspot.com",
+    messagingSenderId: "719056664091"
+};
+firebase.initializeApp(config);
