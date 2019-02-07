16587ea33941a8a0ed53cdcb98a5de92a3640117
Rohan Karmakar <rohankarmakar97@gmail.com>
2019-02-08 01:00:47 +0530

    temp

--- public/JS/dataadd.js
+++ public/JS/dataadd.js
@@ -8,17 +8,25 @@ function addForm(e) {
     var food_name = document.getElementById("itemname").value.trim();
     var food_desc = document.getElementById("desc").value.trim();
     var food_price = document.getElementById("price").value.trim();
-    
+
     var database = firebase.database();
     var ref = database.ref('Menu');
 
     var data = {
-         FoodName: food_name,
-         FoodDesc: food_desc,
-         FoodPrice: food_price
+        FoodName: food_name,
+        FoodDesc: food_desc,
+        FoodPrice: food_price
+    }
+    // var ref_1 = database.ref('user/user2');//child('user1');
+    // var data1 = {
+    //     username: 'nbacanteen@',
+    //     password: 'nbacanteen@'
     }
 
+
     ref.push(data);
-    alert(food_name+ "  Added");
-    console.log('inside fun..');
+    // ref_1.push(data1);
+    // console.log(data1);
+    alert(food_name + "  Added");
+    // console.log('inside fun..');
 }
--- public/JS/main.js
+++ public/JS/main.js
@@ -1,23 +1,40 @@
-function feedbackValidation() {
-    var cust_name=document.getElementById("name").value.trim();
-    var cust_email=document.getElementById("email").value.trim();
-    var cust_feedback=document.getElementById("feedback").value.trim();
-}
+document.getElementById('loginform').addEventListener('submit', adminValidation);
 
-function adminValidation() {
-    const def_username = "nbacanteen@";
-    const def_password = "nbacanteen@";
+function adminValidation(e) {
+    e.preventDefault();
+    var def_password;
+    var def_username;
+    // const def_username = "nbacanteen@";
+    // const def_password = "nbacanteen@";
     //Values extracted from frontend
-    var username = document.getElementById("user").value.trim();
-    //var username = document.getElementById("user");
-    var password = document.getElementById("pass").value.trim();
+    var username = document.getElementById("user");
+    var password = document.getElementById("pass");
 
+    var ref = firebase.database().ref('user');
+    ref.on('value', function(snapshot){
+        var user=snapshot.val();
+        var key=Object.keys(user);
+        console.log(key);
+        var i=key[0];
+        def_password=user[i].password;
+        def_username=user[i].username;
+       // console.log(username+password);
+        console.log(def_password+def_username);
+    });
     //password verification
     if (username == def_username && password == def_password) {
-        return true;
+        location.replace("mainpage.html");
     }
     else {
         alert("Username & password mismatch");
-        return false;
+        location.reload();
     }
 }
+
+// var ref = firebase.database().ref();
+
+// ref.on("value", function(snapshot) {
+//    console.log(snapshot.val());
+// }, function (error) {
+//    console.log("Error: " + error.code);
+// });
